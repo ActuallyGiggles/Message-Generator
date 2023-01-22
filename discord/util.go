ba4bc78749e73f72c587e63de2abbc482e6a0cc8
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-01-22 06:06:43 -0800

    tweet from participation channel
    Modified manuallyTweet function to support messages sent to participation channels.

--- discord/util.go
+++ discord/util.go
@@ -15,48 +15,39 @@ func WrapInCodeBlock(message string) string {
 	return "```" + message + "```"
 }
 
-// memoryMonitor will send a message into error-tracking if the memory usage is exceeded a certain amount.
-// func memoryMonitor() {
-// 	for range time.Tick(10 * time.Second) {
-// 		mem := stats.MemUsage()
-
-// 		allocated := strconv.Itoa(int(mem.Allocated))
-// 		system := strconv.Itoa(int(mem.System))
-
-// 		if int(mem.Allocated) > 500 || int(mem.System) > 5000 {
-// 			SayMention("error-tracking", "<@247905755808792580>", "> Memory usage is exceeding expectations! \n > \n > Allocated -> `"+allocated+"` \n > System -> `"+system+"`")
-// 		}
-// 	}
-// }
-
 func manuallyTweet(r *discordgo.MessageReactionAdd) {
-	var channel string
-	var message string
-
 	// If message was sent by bot
 	messageInfo, err := discord.ChannelMessage(r.ChannelID, r.MessageID)
 	if err != nil {
 		stats.Log(err.Error())
 		return
 	}
+
 	if messageInfo.Author.ID != global.DiscordBotID {
 		return
 	}
 
-	// If starts with "```Channel", get channel from message
-	// Else, get channel from channel name
-	if messageInfo, _ := discord.ChannelMessage(r.ChannelID, r.MessageID); strings.HasPrefix(messageInfo.Content, "```Channel") {
-		s := strings.Split(strings.ReplaceAll(messageInfo.Content, "\n", " "), " ")
-		channel = strings.ReplaceAll(s[1], "\n", "")
+	var channel string
+	var message string
+
+	// If starts with "```Channel:", get channel and message
+	// If starts with "```Channel Used:", get channel and message differently
+	// Else, get channel from channel name and message from content
+	if strings.HasPrefix(messageInfo.Content, "```Channel:") {
+		s := strings.Split(strings.ReplaceAll(strings.ReplaceAll(messageInfo.Content, "`", ""), "\n", " "), " ")
+		channel = s[1]
 		message = strings.Join(s[3:], " ")
-		message = strings.ReplaceAll(message, "```", "")
-		twitter.SendTweet(channel, message)
+	} else if strings.HasPrefix(messageInfo.Content, "```Channel Used:") {
+		s := strings.Split(strings.ReplaceAll(strings.ReplaceAll(messageInfo.Content, "`", ""), "\n", " "), " ")
+		channel = s[2]
+		message = strings.Join(s[8:], " ")
 	} else {
 		c, _ := discord.Channel(r.ChannelID)
 		channel = c.Name
 		message = strings.ReplaceAll(messageInfo.Content, "`", "")
-		twitter.SendTweet(channel, message)
 	}
+
+	twitter.SendTweet(channel, message)
 }
 
 func findChannelIDs(mode string, platform string, channelName string, returnChannelID string) (platformChannelID string, discordChannelID string, success bool) {
