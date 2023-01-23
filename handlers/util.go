7cfc6755448d4d2bcaa69151ee233535a7da0672
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-01-22 19:18:59 -0800

    Update util.go

--- handlers/util.go
+++ handlers/util.go
@@ -5,6 +5,7 @@ import (
 	"Message-Generator/global"
 	"Message-Generator/platform"
 	"Message-Generator/platform/twitch"
+	"fmt"
 	"regexp"
 	"strings"
 	"time"
@@ -14,9 +15,11 @@ import (
 
 // prepareMessageForMarkov prepares the message to be inputted into a Markov chain.
 func prepareMessageForMarkov(msg platform.Message) (processed string) {
+	fmt.Println("test1", msg.Content)
 	processed = removeMentions(msg.Content)
 	processed = lowercaseIfNotEmote(msg.ChannelName, processed)
 	processed = removeWeirdTwitchCharactersAndTrim(processed)
+	fmt.Println("test2", processed)
 	return processed
 }
 
