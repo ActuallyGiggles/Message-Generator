8240ce9bced3657056b10bb1a9bfc236e71b7c6b
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-01-26 10:44:08 -0800

    Update start.go

--- markov/start.go
+++ markov/start.go
@@ -27,32 +27,32 @@ func Start(sI StartInstructions) {
 
 func tickerLoops() {
 	var writingTicker *time.Ticker
-	var zippingTicker *time.Ticker
-	var defluffTicker *time.Ticker
+	// var zippingTicker *time.Ticker
+	// var defluffTicker *time.Ticker
 
 	writingTicker = writeTicker()
 
-	if instructions.ShouldZip {
-		zippingTicker = time.NewTicker(zipInterval)
-		stats.NextZipTime = time.Now().Add(zipInterval)
-	}
+	// if instructions.ShouldZip {
+	// 	zippingTicker = time.NewTicker(zipInterval)
+	// 	stats.NextZipTime = time.Now().Add(zipInterval)
+	// }
 
-	if instructions.ShouldDefluff {
-		defluffTicker = time.NewTicker(defluffInterval)
-		stats.NextDefluffTime = time.Now().Add(defluffInterval)
-	}
+	// if instructions.ShouldDefluff {
+	// 	defluffTicker = time.NewTicker(defluffInterval)
+	// 	stats.NextDefluffTime = time.Now().Add(defluffInterval)
+	// }
 
 	for {
 		select {
 		case <-writingTicker.C:
 			go writeLoop()
 			stats.NextWriteTime = time.Now().Add(writeInterval)
-		case <-zippingTicker.C:
-			go zipChains()
-			stats.NextZipTime = time.Now().Add(zipInterval)
-		case <-defluffTicker.C:
-			go defluff()
-			stats.NextDefluffTime = time.Now().Add(defluffInterval)
+			// case <-zippingTicker.C:
+			// 	go zipChains()
+			// 	stats.NextZipTime = time.Now().Add(zipInterval)
+			// case <-defluffTicker.C:
+			// 	go defluff()
+			// 	stats.NextDefluffTime = time.Now().Add(defluffInterval)
 		}
 	}
 }
